9f1a81cfd3fa7519e36e8d92af84fd5f8012f0df
Lukas Balles <lukas.balles@gmail.com>
2016-11-28 16:38:14 +0100

    Add CIFAR-10 example

--- examples/run_probls_cifar10.py
+++ examples/run_probls_cifar10.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Run probabilistic line search on a MNIST example.
+Run probabilistic line search on a CIFAR-10 example.
 """
 
 import os
@@ -8,23 +8,23 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('data/mnist', one_hot=True)
 
 from probls.tensorflow_interface.interface_sgd import ProbLSOptimizerSGDInterface
 from probls.line_search import ProbLSOptimizer
 
+import cifar10
+
 #### Specify training specifics here ##########################################
-#from models import mnist_2conv_2dense as model # Comment/uncomment to chose
-from models import mnist_mlp as model           # the model to run 
+from models import cifar10_2conv_3dense as model
 num_steps = 4000
 batch_size = 256
 ###############################################################################
 
 
 # Set up model
-losses, placeholders, variables = model.set_up_model()
-X, y = placeholders
+tf.reset_default_graph()
+images, labels = cifar10.distorted_inputs(batch_size=batch_size)
+losses, variables = model.set_up_model(images, labels)
 
 # Set up ProbLS optimizer
 opt_interface = ProbLSOptimizerSGDInterface()
@@ -35,12 +35,16 @@ opt_ls = ProbLSOptimizer(opt_interface, alpha0=1e-3, cW=0.3, c1=0.05,
     target_df=0.5, df_lo=-0.1, df_hi=1.1, expl_policy="linear", fpush=1.0,
     max_change_factor=10., max_steps=10, max_expl=10, max_dmu0=0.0)
 
-# Initialize variables
+# Initialize variables and start queues
+coord = tf.train.Coordinator()
 sess.run(tf.initialize_all_variables())
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
 # Run ProbLS
-batch = mnist.train.next_batch(batch_size)
-opt_ls.prepare({X: batch[0], y: batch[1]})
+opt_ls.prepare()
 for i in range(num_steps):
-  batch = mnist.train.next_batch(batch_size)
-  print(opt_ls.proceed({X: batch[0], y: batch[1]}))
+  print(opt_ls.proceed())
+
+# Stop queues
+coord.request_stop()
+coord.join(threads)
