b98fe3c14a336044a1eef17cda5953cf6b1170a4
Lukas Balles <lukas.balles@gmail.com>
2016-11-28 14:17:24 +0100

    Add MNIST CNN example

--- examples/run_probls_mnist.py
+++ examples/run_probls_mnist.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Run probabilistic line search on an MNIST CNN in interactive mode. Displays
-visualizations of function value f, derivative df, and the probabilistc wolfe
-conditions and expected improvement. Click to proceed one function evaluation.
+Run probabilistic line search on an MNIST CNN.
 """
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('data/mnist', one_hot=True)
@@ -20,8 +17,9 @@ from probls.line_search import ProbLSOptimizer
 
 #### Specify training specifics here ##########################################
 num_steps = 4000
-batch_size = 128
+batch_size = 256
 ###############################################################################
+
 
 # Set up model
 losses, placeholders, variables = model.set_up_model()
@@ -33,24 +31,13 @@ opt_interface.minimize(losses, variables)
 sess = tf.Session()
 opt_interface.register_session(sess)
 sess.run(tf.initialize_all_variables())
-opt_ls = ProbLSOptimizer(opt_interface, cW=0.3, c1=1e-8, target_df=0.5,
-                         df_lo=-0.1, df_hi=1.1, expl_policy="linear",
-                         fpush=1.0, max_change_factor=20., max_steps=10,
-                         max_expl=10, max_dmu0=100.0)
+opt_ls = ProbLSOptimizer(opt_interface, alpha0=1e-3, cW=0.3, c1=0.05,
+    target_df=0.5, df_lo=-0.1, df_hi=1.1, expl_policy="linear", fpush=1.0,
+    max_change_factor=10., max_steps=10, max_expl=10, max_dmu0=0.0)
 batch = mnist.train.next_batch(batch_size)
 opt_ls.prepare({X: batch[0], y: batch[1]})
 
-# Run
-plt.figure()
+# Run ProbLS
 for i in range(num_steps):
-  plt.clf()
   batch = mnist.train.next_batch(batch_size)
-  print opt_ls.proceed({X: batch[0], y: batch[1]})
-  opt_ls.gp.visualize_f(plt.subplot(3, 1, 1))
-  plt.subplot(3, 1, 1).set_ylabel("f")
-  opt_ls.gp.visualize_df(plt.subplot(3, 1, 2))
-  plt.subplot(3, 1, 2).set_ylabel("df")
-  opt_ls.visualize_ei_pw(plt.subplot(3, 1, 3))
-  plt.subplot(3, 1, 3).set_ylabel("p_Wolfe / EI")
-  plt.show()
-  plt.waitforbuttonpress()
+  print(opt_ls.proceed({X: batch[0], y: batch[1]}))
